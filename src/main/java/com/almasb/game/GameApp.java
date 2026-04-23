e1c5074a28cc6432aace63336fbbfcfb5308ef1e
DotHashir <dothashir@gmail.com>
2026-04-23 12:34:12 +0500

    Setting up FXGL in the project

---
+++ src/main/java/com/almasb/game/GameApp.java
@@ -0,0 +1,16 @@
+package com.almasb.game;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class GameApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
