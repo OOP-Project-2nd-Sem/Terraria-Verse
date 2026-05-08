7462830809a3376aac690255fd95697955b51778
DotHashir <dothashir@gmail.com>
2026-05-09 00:24:49 +0500

    Fixed a bug where the enemy would get stuck when moving over flat ground. Enemies can now also 1 block heights

--- src/main/java/com/almasb/game/EnemyComponent.java
+++ src/main/java/com/almasb/game/EnemyComponent.java
@@ -13,6 +13,8 @@ public class EnemyComponent extends Component {
     private double moveSpeed;
     private double damage;
 
+    private double stuckTimer = 0;
+
     public EnemyComponent(int maxHealth, double moveSpeed, double damage) {
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
@@ -33,16 +35,21 @@ public class EnemyComponent extends Component {
 
         //Move towards the player
         if (player != null) {
+            boolean isGrounded = Math.abs(physics.getVelocityY()) < 0.1;
+            boolean isStuck = Math.abs(physics.getVelocityX()) < 0.1;
+
+            //Tracks how long the enemy has been stuck
+            if (isStuck) stuckTimer += tpf;
+            else stuckTimer = 0;
+
             physics.setVelocityX(player.getX() > entity.getX() ? moveSpeed : -moveSpeed);
-        }
 
-        if(physics.getVelocityX() == 0 && isGrounded()) {
-            physics.setVelocityY(-150);
+            //Prevent jumps due to single frame zero velocity
+            if(stuckTimer > 0.2 && isGrounded) {
+                physics.setVelocityY(-170);
+                stuckTimer = 0;
+            }
         }
-    }
-
-    public boolean isGrounded() {
-        return Math.abs(physics.getVelocityY()) < 0.1;
     }
 
     public double getDamage() {return damage;}
